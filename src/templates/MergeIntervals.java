f5a99945dc5e4abb923505fc964a3382ff5df4d5
KaveenE <enkaveen999@gmail.com>
2022-07-11 12:29:38 +0800

    Changed merge intervals tempate abit

--- src/templates/MergeIntervals.java
+++ src/templates/MergeIntervals.java
@@ -6,20 +6,19 @@ import java.util.List;
 
 public class MergeIntervals {
 	public List<int[]> merge(int[][] intervals) {
-		
 		Arrays.sort(intervals, (first, second) -> first[0] - second[0]);
 
 		List<int[]> mergedIntervals = new ArrayList<>();
 		// first element in intervals as baseline
 		int[] mergedInterval = intervals[0];
 
-		for (int row = 0; row <= intervals.length - 2; row++) {
+		for (int row = 1; row <= intervals.length - 1; row++) {
 
-			if (intervals[row + 1][0] <= mergedInterval[1]) {
-				mergedInterval[1] = Math.max(intervals[row + 1][1], mergedInterval[1]);
+			if (mergedInterval[1] >= intervals[row][0]) {
+				mergedInterval[1] = Math.max(intervals[row][1], mergedInterval[1]);
 			} else {
 				mergedIntervals.add(mergedInterval);
-				mergedInterval = intervals[row + 1];
+				mergedInterval = intervals[row];
 			}
 
 		}
