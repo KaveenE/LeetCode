ddb83905e91820e5769e79619aa2fea3c4cfb7f5
Kaveen E <59110376+KaveenE@users.noreply.github.com>
2022-07-22 18:07:56 +0800

    Typo in spiral matrix

--- src/medium/Id54.java
+++ src/medium/Id54.java
@@ -11,19 +11,19 @@ public class Id54 {
         int rowStart = 0,  rowEnd = row - 1;
         int colStart = 0, colEnd = col - 1;
         while (res.size() < row * col) {
-            for (int j = colStart; j <= colEnd; j++)
+            for (int j = colStart; j <= colEnd && res.size()<row*col; j++)
                 res.add(matrix[rowStart][j]);
             rowStart++;
             
-            for (int i = rowStart; i <= rowEnd; i++)
+            for (int i = rowStart; i <= rowEnd && res.size()<row*col; i++)
                 res.add(matrix[i][colEnd]);
             colEnd--;
             
-            for (int j = colEnd; j >= colStart; j--)
+            for (int j = colEnd; j >= colStart && res.size()<row*col; j--)
                 res.add(matrix[rowEnd][j]);
             rowEnd--;
             
-            for (int i = rowEnd; i >= rowStart; i--) 
+            for (int i = rowEnd; i >= rowStart && res.size()<row*col; i--) 
                 res.add(matrix[i][colStart]);
              colStart++;   
 
