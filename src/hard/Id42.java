5c817224a4f25bd01dcadc559831c8f70aa020c5
KaveenE <enkaveen999@gmail.com>
2022-07-22 19:52:21 +0800

    Understanding of trapping rainwater achieved

--- src/hard/Id42.java
+++ src/hard/Id42.java
@@ -1,30 +1,29 @@
 package hard;
 
 /*
-1. Compared to rectangle qn, need to find firstIdxMoreThanCurrHeight from left and right instead of less
-	i. Actually, we need to store the actual value rather than index thus 
-	   the our first 2 for loops differ from rectangle problem
+1. Compared to rectangle qn, need to find biggestHeight on left and right instead of less 
+    (also note it does NOT have to be the FIRST height bigger than current height)
 2. Compared to rectangle qn, height is not restricted by height of ith rectangle and can rise above.
 	i. Thus, the condition if(waterLevel>=heights[i])trappedWater+=waterLevel-heights[i]; is necessary
 */
 public class Id42 {
     public int trap(int[] heights) {
-        int[] firstHeightMoreThanFromLeft = new int[heights.length];
-        int[] firstHeightMoreThanFromRight = new int[heights.length];
+        int[] biggestHeightOnLeft = new int[heights.length];
+        int[] biggestHeightOnRight = new int[heights.length];
         
         for(int i=1; i<=heights.length-1; i++){
-            firstHeightMoreThanFromLeft[i]=Math.max(firstHeightMoreThanFromLeft[i-1],heights[i-1]);  
+            biggestHeightOnLeft[i]=Math.max(biggestHeightOnLeft[i-1],heights[i-1]);  
         }
         
         for(int i=heights.length-2; i>=0; i--){
-            firstHeightMoreThanFromRight[i]=Math.max(firstHeightMoreThanFromRight[i+1],heights[i+1]); 
+            biggestHeightOnRight[i]=Math.max(biggestHeightOnRight[i+1],heights[i+1]); 
         }
         
         
         int trappedWater = 0;
         int waterLevel = 0;
         for(int i=0; i<=heights.length-1; i++){
-            waterLevel = Math.min(firstHeightMoreThanFromLeft[i], firstHeightMoreThanFromRight[i]); 
+            waterLevel = Math.min(biggestHeightOnLeft[i], biggestHeightOnRight[i]); 
             if(waterLevel>=heights[i])trappedWater+=waterLevel-heights[i];
         }
         
