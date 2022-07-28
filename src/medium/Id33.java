0716b0ed0d0ff94b7aeb155f5779096597531e1f
KaveenE <enkaveen999@gmail.com>
2022-07-28 11:54:02 +0800

    Used 2 generalized binary search for "Search in Rotated Sorted Array"

--- src/medium/Id33.java
+++ src/medium/Id33.java
@@ -1,33 +1,37 @@
 package medium;
 
 public class Id33 {
-	public int search(int[] nums, int target) {
+    public int search(int[] nums, int target) {
 
-		int left = 0, right = nums.length - 1;
-		int mid = 0;
-		while (right > left) {
-			mid = left + (right - left) / 2;
+        int left = 0, right = nums.length - 1;
+        int mid = 0;
+        while (right > left) {
+            mid = left + (right - left) / 2;
 
-			// If both target and nums[mid] on the same half, do standard binary search
-			if ((nums[mid] - nums[nums.length - 1]) * (target - nums[nums.length - 1]) > 0) {
+            //If both target and nums[mid] on same side of rotation, do standard binary search
+            if ((nums[mid] - nums[nums.length - 1]) * (target - nums[nums.length - 1]) > 0) {
+                if (targetLessEqualMid(mid, target, nums))
+                    right = mid;
+                else
+                    left = mid + 1;
 
-				if (target == nums[mid])
-					return mid;
-				else if (target > nums[mid])
-					left = mid + 1;
-				else
-					right = mid;
+            }
+            //target on left left, so ignore right half
+            else if (targetOnLeftHalf(target, nums)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
 
-			}
-			// target on left left, so ignore right half
-			else if (target > nums[nums.length - 1]) {
-				right = mid;
-			} else {
-				left = mid + 1;
-			}
-		}
+        return (nums[left] == target) ? left : -1;
+    }
 
-		// Post-processing where left == right
-		return (left != nums.length && nums[left] == target) ? left : -1;
-	}
+    public boolean targetLessEqualMid(int mid, int target, int[] nums) {
+        return target <= nums[mid];
+    }
+
+    public boolean targetOnLeftHalf(int target, int[] nums) {
+        return target > nums[nums.length - 1];
+    }
 }
