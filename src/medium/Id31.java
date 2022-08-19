ce42f90eff6841600c187236449a9999dc6e1629
LAPTOP-EF8UUDBS\enkav <enkaveen999@gmail.com>
2022-08-19 16:02:26 +0800

    both copied from soln, read comments...

---
+++ src/medium/Id31.java
@@ -0,0 +1,61 @@
+package medium;
+
+import java.util.*;
+
+/*
+To find next lexigraphically larger perm
+
+1. I need to find idx of rightmost elem,i whose right neighbor is larger
+	1i. If no such idx:
+	-> we already have the lexiographically largest perm (descending)
+	-> thus we rotate back to lexiographically smallest permutation (ascending)
+	
+	1ii. Else:
+	-> Find idx of rightmost elem, r s.t arr[r]>arr[i] && r>i
+	-> Swap arr[r] and arr[i]
+      -> Reverse arr[i:]
+*/
+class Id31 {
+    public void nextPermutation(int[] nums) {
+        
+        int rightMostIdxWithLargerRightNeighbor = -1;
+        for(int idx=nums.length-2; idx>=0; idx--){
+            if(nums[idx+1]>nums[idx]){
+                rightMostIdxWithLargerRightNeighbor =idx;
+                break;
+            }
+        }
+        
+        if(rightMostIdxWithLargerRightNeighbor == -1){
+            Arrays.sort(nums);
+        }
+        else{
+            int rightMostIdxOfElemLargerThanElemAbove = -1;
+            for(int idx=nums.length-2; idx>=0; idx--){
+                if(nums[idx+1]>nums[rightMostIdxWithLargerRightNeighbor]){
+                    rightMostIdxOfElemLargerThanElemAbove =idx+1;
+                    break;
+                }
+            }
+            
+            swap(rightMostIdxWithLargerRightNeighbor, rightMostIdxOfElemLargerThanElemAbove, nums);
+
+            int offsetFromEnd = 0;
+            for(int i=rightMostIdxWithLargerRightNeighbor+1
+                ;i<= (nums.length-1 + rightMostIdxWithLargerRightNeighbor+1)/2 
+                ;i++){
+                swap(i,nums.length-1-offsetFromEnd, nums);
+                offsetFromEnd++;
+            }
+            
+        }
+        
+    }
+    
+    private void swap(int idx1, int idx2, int[] arr){
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+        
+    }
+}
---
+++ src/medium/Id49.java
@@ -0,0 +1,30 @@
+package medium;
+
+import java.util.*;
+
+/*
+To check for anagrams, instead of the traditional way of 
+1) caching frequency of 1 string and decrementing the frequency in the other 
+   to see if all reaches 0
+2) You can sort both strings and see if they are equal
+
+Both  are space O(N), time O(N)
+
+In this 
+*/
+class Id49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> sortedStringToRealString = new HashMap<>();
+        
+        for(String str: strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String strConverted = String.valueOf(charArray);
+            
+            sortedStringToRealString.computeIfAbsent(strConverted, _key->new ArrayList<>());
+            sortedStringToRealString.get(strConverted).add(str);
+        }
+        
+        return new ArrayList<>(sortedStringToRealString.values());
+    }
+}
