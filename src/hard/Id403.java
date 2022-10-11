2d75d1b129d42102a513d2f18d1adf6e14d8dfc2
LAPTOP-EF8UUDBS\enkav <enkaveen999@gmail.com>
2022-10-11 18:39:12 +0800

    Renamed variable for clarity

--- src/hard/Id403.java
+++ src/hard/Id403.java
@@ -7,7 +7,7 @@ class Id403 {
         return canJump(stones, 0, 1, canJumpFromStoneI);
     }
     
-    public boolean canJump(int[] stones, int stackIdx, int k, Boolean[][] canJumpFromStoneI){
+    public boolean canJump(int[] stones, int stackIdx, int jumps, Boolean[][] canJumpFromStoneI){
         if(stackIdx>stones.length-1){
             return false;
         }
@@ -16,32 +16,32 @@ class Id403 {
         }
         
         
-        if(canJumpFromStoneI[k][stackIdx]!=null)return canJumpFromStoneI[k][stackIdx];
+        if(canJumpFromStoneI[jumps][stackIdx]!=null)return canJumpFromStoneI[jumps][stackIdx];
         
         boolean canJump=false;
         for(int i=stackIdx+1; i<=stones.length-1; i++){
-            if(stones[i]-stones[stackIdx]>k+1)continue;
+            if(stones[i]-stones[stackIdx]>jumps+1)continue;
             
             //Must only try jump of 1 unit
             if(stackIdx==0){
-                if(stones[i]-stones[stackIdx]==k){
-                    canJump=canJump || canJump(stones, i, k, canJumpFromStoneI);
+                if(stones[i]-stones[stackIdx]==jumps){
+                    canJump=canJump || canJump(stones, i, jumps, canJumpFromStoneI);
                 }
             }else {
-                if(stones[i]-stones[stackIdx]==k){
-                    canJump=canJump || canJump(stones, i, k, canJumpFromStoneI);
+                if(stones[i]-stones[stackIdx]==jumps){
+                    canJump=canJump || canJump(stones, i, jumps, canJumpFromStoneI);
                 }
-                else if(stones[i]-stones[stackIdx]==k-1){
-                    canJump=canJump || canJump(stones, i, k-1, canJumpFromStoneI);
+                else if(stones[i]-stones[stackIdx]==jumps-1){
+                    canJump=canJump || canJump(stones, i, jumps-1, canJumpFromStoneI);
                 }
-                else if(stones[i]-stones[stackIdx]==k+1){
-                    canJump=canJump || canJump(stones, i, k+1, canJumpFromStoneI);
+                else if(stones[i]-stones[stackIdx]==jumps+1){
+                    canJump=canJump || canJump(stones, i, jumps+1, canJumpFromStoneI);
                 }
             }  
 
         }
         
-        return canJumpFromStoneI[k][stackIdx] = canJump;
+        return canJumpFromStoneI[jumps][stackIdx] = canJump;
     }
 }
 
