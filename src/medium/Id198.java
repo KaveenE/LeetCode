9d4653470e1016c28f2f379cb7c9b390b703cf8b
KaveenE <enkaveen999@gmail.com>
2022-10-18 13:46:40 +0800

    Edited 198. House Robber - Made code reflect my notes more closely - the part I wanted to it to reflect was the choose X or not Choose X/choose Y

--- src/medium/Id198.java
+++ src/medium/Id198.java
@@ -5,17 +5,21 @@ public class Id198 {
         Integer[] maxRobbedStartingFromHse = new Integer[nums.length];
         return maxRobbed(0, nums, maxRobbedStartingFromHse);
     }
-    
+
     public int maxRobbed(int toRobHseIdx, int[] nums, Integer[] maxRobbedStartingFromHse){
         if(toRobHseIdx>nums.length-1){
             return 0;
         }
-        
+
         if(maxRobbedStartingFromHse[toRobHseIdx]!=null)return maxRobbedStartingFromHse[toRobHseIdx];
-        
+
         int moneyRobbedAdjHse1 = nums[toRobHseIdx] + maxRobbed(toRobHseIdx+2, nums, maxRobbedStartingFromHse);
-        int moneyRobbedAdjHse2 = maxRobbed(toRobHseIdx+1, nums, maxRobbedStartingFromHse);
-        
+        int moneyRobbedAdjHse2 = 0;
+        if(toRobHseIdx+1<=nums.length-1){
+            moneyRobbedAdjHse2 = nums[toRobHseIdx+1] + maxRobbed(toRobHseIdx+3, nums, maxRobbedStartingFromHse);
+        }
+
+
         return maxRobbedStartingFromHse[toRobHseIdx] = Math.max(moneyRobbedAdjHse1,moneyRobbedAdjHse2);
     }
 }
