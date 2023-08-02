2c8854f27f4ea4fe315a07a206ea97b7871ca121
Kaveen E <59110376+KaveenE@users.noreply.github.com>
2023-08-02 09:52:44 +0800

    Better explanation for Max Chunks, Id769.java

--- src/medium/Id769.java
+++ src/medium/Id769.java
@@ -1,28 +1,32 @@
 package medium;
 
 /*
-To get max chunks, we need to know max number of continuous sequence
-IE 1 continuous sequeunce = 1 chunk
+Input array arr is a permutation of the integers in the range [0, n-1], where n is the length of the array. 
+This means that the elements of the array are distinct and include all integers from 0 to n-1, exactly once.
 
-Eg:
-array: [1, 0, 2, 4, 3]
-cut :  [1, 0 | 2 | 4 | 3]
-[1, 0], [2], [4], [3] give 4 continuous sequences and thus 4 chunks
+We want to split the array into chunks, sort each chunk, and then concatenate them to get the sorted array.
 
-A continuous sequence has formed when the maxNumSeen == numInCurrIdxOfSortedArray
-Since arr's values lie in [0,n-1], numInCurrIdxOfSortedArray is same as the currIdx itself
+Key observation: 
+In a sorted array of distinct integers from 0 to n-1, each element i will be at its correct index i. 
+Eg: In [0, 1, 2, 3, 4], element 0 is at index 0, element 1 is at index 1, and so on.
+
+Let's use this obeservation 
+- Iterate array from left to right while keeping track of max val seen so far (maxValueSeen). 
+- If the current index i is equal to the maxValueSeen, 
+  - Means that all the elements from 0 to maxValueSeen have been encountered so far, and they are correctly placed at their corresponding indices. 
+  - So, we have found a chunk that can be sorted
 */
 class Id769 {
     public int maxChunksToSorted(int[] arr) {
-        int maxContSeq=0;
-        int maxNumSeen = 0;
+        int maxChunks = 0;
+        int maxValueSeen = 0;
         
-        for (int idx=0; idx<=arr.length-1; idx++) {
-            maxNumSeen = Math.max(maxNumSeen, arr[idx]);
-            if (maxNumSeen == idx) maxContSeq++;
+        for (int i = 0; i < arr.length; i++) {
+            maxValueSeen = Math.max(maxValueSeen, arr[i]);
             
+            if(maxValueSeen == i) maxChunks++;
         }
         
-        return maxContSeq;
+        return maxChunks;
     }
 }
